26189f4c939adb73d94f406dd171e764a8dec4f3
Xabier Aresti <xabier.aresti@gmail.com>
2021-03-21 17:29:31 +0100

    Initial Commit

---
+++ src/main/java/com/couponet/app/CoupoNetAppApplication.java
@@ -0,0 +1,13 @@
+package com.couponet.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupoNetAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoupoNetAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/couponet/app/CoupoNetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponet.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupoNetAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
