cddc7968afe92a150a86b53bb78c38daa3b5c149
Xabier Aresti <xabier.aresti@gmail.com>
2021-05-23 13:01:14 +0200

    IMPORTANT CHANGES - SERVER IMPLEMENTATION

--- src/main/java/com/couponet/app/CoupoNetAppApplication.java
+++ src/main/java/com/couponet/app/CoupoNetAppApplication.java
@@ -2,11 +2,12 @@ package com.couponet.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-public class CoupoNetAppApplication {
+public class CoupoNetAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CoupoNetAppApplication.class, args);
